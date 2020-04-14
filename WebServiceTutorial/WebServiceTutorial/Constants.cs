dfd3ea1fb45ea5484ebc7f1c4f702378a540f1e1
SRMColor <s.r.m.color@gmail.com>
2020-04-14 14:12:29 +0900

    プロジェクト ファイルを追加します。

---
+++ WebServiceTutorial/WebServiceTutorial/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace WebServiceTutorial
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "7772d1b504e125c735dc8c8fcdfafd6f";
+    }
+}
