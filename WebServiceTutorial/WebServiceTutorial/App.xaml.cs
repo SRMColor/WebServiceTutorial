f0894bf8edd43a347cb7ad48129c37d2f32fd599
SRMColor <s.r.m.color@gmail.com>
2020-04-14 15:37:55 +0900

    リリース用２

--- WebServiceTutorial/WebServiceTutorial/App.xaml.cs
+++ WebServiceTutorial/WebServiceTutorial/App.xaml.cs
@@ -2,6 +2,10 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace WebServiceTutorial
 {
     public partial class App : Application
@@ -15,6 +19,10 @@ namespace WebServiceTutorial
 
         protected override void OnStart()
         {
+            AppCenter.Start("ios=027d2cdb-baf1-4aef-a403-13d3316576ae;" +
+                  "uwp={Your UWP App secret here};" +
+                  "android={Your Android App secret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
